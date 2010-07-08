ca4d5b1defee51a7b4b49350357eac3deec09b5e
Matt Wiebe <matt@somadesign.ca>
2010-07-08 10:56:18 -0500

    code cleanup

--- library/epigraph.php
+++ library/epigraph.php
@@ -30,6 +30,7 @@ function erdt_epigraph($post_id = null, $return = false ) {
 	}
 }
 
+add_action('admin_menu', 'erdt_epigraph_meta_box');
 function erdt_epigraph_meta_box() {
 	add_meta_box('erdudte-epigraph', __('Epigraph', 'erudite'), 'erdt_epigraph_meta_box_contents', 'post', 'normal', 'high');
 }
@@ -40,10 +41,10 @@ function erdt_epigraph_meta_box_contents() {
 	
 	<p><label for="epigraph_citation"><?php _e('Citation (optional)', 'erudite') ?>:</label> <input type="text" name="epigraph_citation" value="<?php esc_attr_e( get_post_meta( $post->ID, 'epigraph_citation', true ) ) ?>" id="epigraph_citation"></p>
 	
-	<?php wp_nonce_field('erdt_epigraph', 'erdt_epigraph_nonce', true, true) ?>
-	
-<?php }
+	<?php wp_nonce_field('erdt_epigraph', 'erdt_epigraph_nonce', true, true);
+}
 
+add_action('save_post', 'erdt_epigraph_save');
 function erdt_epigraph_save($post_id) {
 	if ( wp_verify_nonce( $_POST['erdt_epigraph_nonce'], 'erdt_epigraph' ) ) {
 		if ( strlen( $_POST['epigraph'] ) > 0 ) :
@@ -58,9 +59,4 @@ function erdt_epigraph_save($post_id) {
 		endif;
 		
 	}
-}
-
-if ( is_admin() ) {
-	add_action('admin_menu', 'erdt_epigraph_meta_box');
-	add_action('save_post', 'erdt_epigraph_save');
 }
